acb8fb73af1dbf9988b1b4706ad74e002b5198e2
Francois Berenger <unixjunkie@sdf.org>
2019-12-04 15:54:22 +0900

    Slist: WIP

--- src/slist.ml
+++ src/slist.ml
@@ -1,36 +1,36 @@
 
+module A = Array
 module RNG = Random.State
 
 let rng = RNG.make_self_init ()
 
-(* type ('a, 'b) elt *)
+type ('a, 'b) elt = { k: 'a;
+                      v: 'b;
+                      arr: (('a, 'b) elt list) array }
 
-(* type t *)
-
-(* FBR: instead of using cmp, we should use a functor *)
-
-(* (\* probability constant *\)
- * let p = 0.5 *)
+type ('a, 'b) t = (('a, 'b) elt list) array
 
 let max_level = 32
 
 let random_level () =
   let level = ref 0 in
+  (* probability = 0.5 <=> RNG.bool *)
   while RNG.bool rng && !level < max_level do
     incr level
   done;
   !level
 
 let create () =
-  failwith "not implemented yet"
+  A.init max_level (fun _i -> ref [])
+
+(* FBR TODO: instead of using cmp, we should use a functor *)
+let add _cmp _l _x =
+  failwith "not implemented yet"  
 
 let of_list _l =
   failwith "not implemented yet"
 
 let to_list _t =
-  failwith "not implemented yet"
-
-let add _cmp _l _x =
   failwith "not implemented yet"
 
 let at _l _i =
